915c974f75150065cb6ea424abc7887f690514fa
walidbadar <81442784+walidbadar@users.noreply.github.com>
2022-01-06 00:24:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,28 @@ from pytube import YouTube
 from datetime import datetime
 import os, re, time, requests, json
 
+path = "D:/Softwares/Waleed Docs/Projects/Selenium/Youtube Automation/Videos/"
+
 def youtube_video_downloader(topic,linkorder):
     global driver
 
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=options)
     driver.get('https://youtube.com/')
 
     # Search Topic
-    xpath_searchBar = '//input[@id="search"]'
+    xpath_searchBar = '/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input'
     WebDriverWait(driver, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpath_searchBar)))
     uid_input = driver.find_element_by_xpath(xpath_searchBar)
     uid_input.send_keys(topic)
-    driver.implicitly_wait(1)
-    uid_input.send_keys(Keys.ENTER)
+
+    xpath_searchButton = '//*[@id="search-icon-legacy"]'
+    WebDriverWait(driver, 60).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_searchButton)))
+    searchButton = driver.find_element_by_xpath(xpath_searchButton)
+    searchButton.click()
 
     # Click on Filter Button
     xpath_filterButton = '/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/div/ytd-toggle-button-renderer/a/tp-yt-paper-button/yt-icon'
@@ -34,7 +39,7 @@ def youtube_video_downloader(topic,linkorder):
 
     # Use Creative Common Filter
     xpath_creativeCommon = '/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[4]/ytd-search-filter-renderer[5]/a/div/yt-formatted-string'
-    WebDriverWait(driver, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpath_creativeCommon)))
+    WebDriverWait(driver, 60).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_creativeCommon)))
     driver.find_element_by_xpath(xpath_creativeCommon).click()
 
     # Click on Filter Button
@@ -44,7 +49,7 @@ def youtube_video_downloader(topic,linkorder):
 
     # Sort Videos by Upload Date
     xpath_sortButton = '/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[1]/div[2]/ytd-search-sub-menu-renderer/div[1]/iron-collapse/div/ytd-search-filter-group-renderer[5]/ytd-search-filter-renderer[2]/a/div/yt-formatted-string'
-    WebDriverWait(driver, 60).until(expected_conditions.presence_of_element_located((By.XPATH, xpath_sortButton)))
+    WebDriverWait(driver, 60).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_sortButton)))
     driver.find_element_by_xpath(xpath_sortButton).click()
 
     # Search Video Links
@@ -60,13 +65,14 @@ def youtube_video_downloader(topic,linkorder):
     yt = YouTube("https://www.youtube.com/watch?v="+linklist[linkorder]+"\"")
     d_video = yt.streams.get_by_itag(22)
     # d_video.download(filename=str(linkorder)+".mp4")
-    d_video.download('Videos')
+    d_video.download(path)
     print('Video Dowloaded!')
 
 while 1:
     try:
-        youtube_video_downloader("psychology",0)
-        time.sleep(1800)
+        for i in range(20):
+                youtube_video_downloader("psychology",0)
+                time.sleep(1800)
 
     except:
         print("Some Error")
